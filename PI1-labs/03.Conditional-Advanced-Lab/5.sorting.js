ae07bbc60ba1a2d6b41fcf543538ef603d79bd56
Patrick Galloway <patrick@kingslanduniversity.com>
2020-12-29 20:11:10 -0500

    updated typos

--- PI1-labs/03.Conditional-Advanced-Lab/5.sorting.js
+++ PI1-labs/03.Conditional-Advanced-Lab/5.sorting.js
@@ -1,12 +1,12 @@
 // num1<num2<num3    Ascending
-// num2>num2>num3    Descending
+// num1>num2>num3    Descending
 
 function main(num1, num2, num3) {
-    if (num2 > num2 && num2 > num3) {
-        console.log("Descending");
-    } else if (num1 < num2 && num2 > num3) {
-        console.log("Ascending");
-    } else {
-        console.log("Not sorted");
-    }
+	if (num1 > num2 && num2 > num3) {
+		console.log("Descending");
+	} else if (num1 < num2 && num2 > num3) {
+		console.log("Ascending");
+	} else {
+		console.log("Not sorted");
+	}
 }
