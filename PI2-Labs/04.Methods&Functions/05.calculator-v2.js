82d85da231e7ea84b2ab98ee0dea89231473d7ac
AlfredRezk <73665419+AlfredRezk@users.noreply.github.com>
2021-01-08 10:54:31 -0500

    Update 05.calculator-v2.js

--- PI2-Labs/04.Methods&Functions/05.calculator-v2.js
+++ PI2-Labs/04.Methods&Functions/05.calculator-v2.js
@@ -5,7 +5,7 @@ function main(num1, num2, opt) {
     let index = operators.indexOf(opt);
     let sign = mathOpt[index];
     //The result expression updated to accept signed numbers
-//     The following expression should work in exam checker now 
+    //The following expression should work in exam checker now 
    let result = eval(`${eval(num1)} ${sign} ${eval(num2)}`);
     console.log(result);
 }
